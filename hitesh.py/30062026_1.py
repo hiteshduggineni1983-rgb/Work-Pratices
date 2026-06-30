6ff5077e7bbec5312c123a3428c46a5937efefd1
Hiiesh Duggineni <hiteshduggineni1983@gmail.com>
2026-06-30 21:28:41 +0530

    Should you go for a walk

---
+++ hitesh.py/30062026_1.py
@@ -0,0 +1,10 @@
+# give condition  
+is_sunny = True
+have_umbrella = False
+
+#results
+
+print(not is_sunny)
+print(not have_umbrella)
+print( is_sunny and not have_umbrella)
+print(is_sunny or have_umbrella)
