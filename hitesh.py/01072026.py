11cb749c182f5a59518033c122b59bec372174f0
Hiiesh Duggineni <hiteshduggineni1983@gmail.com>
2026-07-01 15:32:29 +0530

    shopping list

---
+++ hitesh.py/01072026.py
@@ -0,0 +1,23 @@
+my_cart = ["apples","bananas","milk"]
+print(my_cart)
+my_cart.append("bread")
+print(my_cart)
+my_cart.insert(0,"ketchup")
+print(my_cart)
+my_cart.remove("bananas")
+print(my_cart)
+removed_item = my_cart.pop()
+print(removed_item)
+my_cart.extend(["rice","butter"])
+print(my_cart)
+my_cart.sort()
+print(my_cart)
+my_cart.reverse()
+print(my_cart)
+my_cart1 = my_cart + ["juice","jam"]
+print(my_cart1)
+my_cart2 = my_cart * 2
+print(my_cart2)
+vegetables =  "tomato cucumber spinach" 
+vegetable_list = vegetables.split(" ")
+print(vegetable_list)
