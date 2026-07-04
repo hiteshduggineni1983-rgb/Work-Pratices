44c69916e88add502cfc47e1f69fb7bf9f35569e
Hiiesh Duggineni <hiteshduggineni1983@gmail.com>
2026-07-04 15:45:31 +0530

    Customer Profile Management

---
+++ hitesh.py/04072026_1.py
@@ -0,0 +1,26 @@
+# Step 1: Create a customer dictionary with name, age, and city
+customer = {
+    "name": "John Doe",
+    "age": 32,
+    "city": "New York"
+}
+print(customer)
+# Step 2: Add email and phone
+customer["email"] == johdoe1919@gmail.com
+customer["phone"] == 9910010004
+print(customer["email"])
+print(customer["phone"])
+# Step 3: Print customer's name and city
+print(customer["name"])
+print(customer["city"])
+
+# Step 4: Check if "email" exists
+get_email = "email" in customer
+print(get_email)
+# Step 5: Delete the "age" field
+del customer["age"]
+print(customer)
+# Step 6: Print all keys, values, and items
+print(keys)
+print(values)
+print(items)
