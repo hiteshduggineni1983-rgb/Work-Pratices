618f323eb7f9f9f3351c12fe05d519a713dba5ea
Hiiesh Duggineni <hiteshduggineni1983@gmail.com>
2026-07-07 08:01:27 +0530

    Customer Discount Eligibility

---
+++ hitesh.py/07072026.py
@@ -0,0 +1,10 @@
+customer_name = "Amit"
+#print(customer_name)
+customer_age = 24
+##print(customer_age)
+total_purchase = 1200
+#print(total_purchase)
+if total_purchase > 1000:
+    print("Eligible for discount")
+if customer_age >= 60:
+    print("senior Citizen")
